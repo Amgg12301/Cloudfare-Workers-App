aad78f4509d79e04c79121dc579bb18421421e26
Amogh Giri <amoghgiri01@gmail.com>
2020-04-20 02:02:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,55 +3,63 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
+  // Request URLs from API
   const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
-  // Send the request on to the origin server.
+  // Send the request to server via Fetch
   const response = await fetch(url)
 
+  // Check if valid response
   if(!response.ok){
     throw response
   }
 
-  // Read response body.
+  // Read and modify response to JSON
   const text = await response.text()
-
-  // Modify it.
   const modified = JSON.parse(text)
   const variantOne = modified["variants"][0]
   const variantTwo = modified["variants"][1]
 
+  // These will be used for the cookie and its data
   const NAME = 'cloudfareFullStack'
   var variant = ''
   const cookie = request.headers.get('cookie')
 
+  // Distribute requests between variants
+  // A/B testing with Cookies
   if (cookie && cookie.includes(`${NAME}={variantOne}`)) {
     variant = variantOne
   } else if (cookie && cookie.includes(`${NAME}={variantTwo}`)) {
     variant = variantTwo
   } else {
-    // if no cookie then this is a new client, decide a group and set the cookie
-    variant = Math.random() < 0.5 ? variantOne : variantTwo // 50/50 split
-    console.log(variant)
+    // No existing cookie --> 50/50 split using A/B testing
+    variant = Math.random() < 0.5 ? variantOne : variantTwo
   }
 
+  // Using selected variant, get its data
   const variantResponse = await fetch(variant)
   const variantText = await variantResponse.text()
   var editedText = ''
+
+  //Changing HTML Code based on Variant
   if(variant == variantOne){
+      // Variant One has a link to my LinkedIn profile! Check it out :)
       editedText = variantText.replace('<title>Variant 1</title>', "<title>Welcome to Cloudfare's Variant One</title>")
       editedText = editedText.replace("Variant 1", "<b>Amogh Giri and Cloudfare Present Variant One!</b>")
       editedText = editedText.replace("This is variant one of the take home project!",
         "<i>Variant One was chosen for you using a cookie and A/B testing!</i>")
       editedText = editedText.replace("https://cloudflare.com", "https://www.linkedin.com/in/amogh-giri01/")
-      editedText = editedText.replace("Return to cloudflare.com", "Let's take a visit to Amogh's LinkedIn")
+      editedText = editedText.replace("Return to cloudflare.com", "Click here to take a visit to Amogh's LinkedIn")
   }else{
+      // Variant Two has a link to my GitHub profile! Check it out :)
       editedText = variantText.replace('<title>Variant 2</title>', "<title>Welcome to Cloudfare's Variant Two</title>")
       editedText = editedText.replace("Variant 2", "<b>Amogh Giri and Cloudfare Present Variant Two!</b>")
       editedText = editedText.replace("This is variant two of the take home project!",
         "<i>Variant Two was chosen for you using a cookie and A/B testing!</i>")
       editedText = editedText.replace("https://cloudflare.com", "https://github.com/Amgg12301")
-      editedText = editedText.replace("Return to cloudflare.com", "Time to check out some cool code Amogh has written!")
+      editedText = editedText.replace("Return to cloudflare.com", "Click here to check out some cool code Amogh has written!")
   }
-  console.log(editedText)
+
+  // Creating response and setting its cookie
   var responseWithCookie = new Response(editedText, {
     headers: { 'content-type': 'text/html' },
   })
